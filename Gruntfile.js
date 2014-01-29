1096d7e6fd65394f38ce599f155f7d689be3125f
Kenneth Endfinger <kaendfinger@gmail.com>
2014-01-28 21:08:09 -0500

    CSSLint!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,70 +1,81 @@
 /*global module:false*/
 var sources = [
-    "src/js/hwinfo.js"
+   "src/js/hwinfo.js"
 ];
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        // Task configuration.
-        jshint: {
-            options: {
-                curly: false,
-                eqeqeq: true,
-                immed: true,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                undef: true,
-                unused: true,
-                boss: true,
-                eqnull: true,
-                browser: true,
-                globals: {
-                    "jQuery": false,
-                    "$": false,
-                    "chrome": false,
-                    "bootbox": false
-                }
-            },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
-            hwinfo: {
-                src: sources
+   // Project configuration.
+   grunt.initConfig({
+      // Task configuration.
+      jshint: {
+         options: {
+            curly: false,
+            eqeqeq: true,
+            immed: true,
+            latedef: true,
+            newcap: true,
+            noarg: true,
+            sub: true,
+            undef: true,
+            unused: true,
+            boss: true,
+            eqnull: true,
+            browser: true,
+            globals: {
+               "jQuery": false,
+               "$": false,
+               "chrome": false,
+               "bootbox": false
             }
-        },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            hwinfo: {
-                files: sources,
-                tasks: ['jshint:hwinfo']
-            }
-        },
-        crx: {
-            hwinfo: {
-                src: "src/",
-                dest: "build/crx",
-                privateKey: "~/.ssh/chrome-apps.pem"
-            }
-        }
-    });
+         },
+         gruntfile: {
+            src: 'Gruntfile.js'
+         },
+         hwinfo: {
+            src: sources
+         }
+      },
+      watch: {
+         gruntfile: {
+            files: '<%= jshint.gruntfile.src %>',
+            tasks: ['jshint:gruntfile']
+         },
+         hwinfo: {
+            files: sources,
+            tasks: ['jshint:hwinfo']
+         }
+      },
+      crx: {
+         hwinfo: {
+            src: "src/",
+            dest: "build/crx",
+            privateKey: "~/.ssh/chrome-apps.pem"
+         }
+      },
+      csslint: {
+         options: {
+            // No Vendor Prefixes. We only will be using Blink-based browsers.
+            "compatible-vendor-prefixes": false,
+            "vendor-prefix": false
+         },
+         hwinfo: {
+            src: ['src/css/style.css']
+         }
+      }
+   });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-crx');
+   grunt.loadNpmTasks('grunt-contrib-jshint');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+   grunt.loadNpmTasks('grunt-contrib-csslint');
+   grunt.loadNpmTasks('grunt-crx');
 
-    // Default task.
-    grunt.registerTask('default', ['jshint']);
+   // Default task.
+   grunt.registerTask('default', ['jshint', 'csslint']);
 
-    // Test task.
-    grunt.registerTask('test', ['jshint']);
+   // Test task.
+   grunt.registerTask('test', ['jshint', 'csslint']);
 
-    // Release task.
-    grunt.registerTask('release', ['jshint', 'crx']);
+   // Release task.
+   grunt.registerTask('release', ['jshint', 'crx']);
 };
